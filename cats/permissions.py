0bdcd74577397a7ae36f7c3a869c60e1bf3db65f
Игорь Жук <igor.zhuk.code@gmail.com>
2022-10-04 21:39:33 +0400

    Add trottling, pagination and filters.

--- cats/permissions.py
+++ cats/permissions.py
@@ -10,10 +10,13 @@ class OwnerOrReadOnly(permissions.BasePermission):
         )
 
     def has_object_permission(self, request, view, obj):
-        return obj.owner == request.user
+        return (
+                obj.owner == request.user
+                or request.method in permissions.SAFE_METHODS
+        )
 
 
-class ReadOnly(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        return request.method in permissions.SAFE_METHODS
+# class ReadOnly(permissions.BasePermission):
+#
+#     def has_permission(self, request, view):
+#         return request.method in permissions.SAFE_METHODS
--- cats/views.py
+++ cats/views.py
@@ -1,22 +1,38 @@
 from rest_framework import viewsets
+from rest_framework import filters
+from rest_framework.throttling import ScopedRateThrottle
 
 from .models import Achievement, Cat, User
-from .permissions import OwnerOrReadOnly, ReadOnly
+from .permissions import OwnerOrReadOnly  # ReadOnly
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
+from .throttling import WorkingHoursRateThrottle
+from .pagination import CatsPagination
+
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
     permission_classes = (OwnerOrReadOnly,)
+    throttle_classes = (ScopedRateThrottle, WorkingHoursRateThrottle)
+    throttle_scope = 'low_request'
+    filter_backends = (
+        DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter
+    )
+    pagination_class = None  # TODO CatsPagination
+    filterset_fields = ('color', 'birth_year')
+    search_fields = ('name',)
+    ordering_fields = ('name', 'birth_year')
+    ordering = ('birth_year',)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-    def get_permissions(self):
-        if self.action == 'retrieve':
-            return (ReadOnly(),)
-        return super().get_permissions()
+    # def get_permissions(self):
+    #     if self.action == 'retrieve':
+    #         return (ReadOnly(),)
+    #     return super().get_permissions()
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
